84552c0be355d7edbc215f42e7739833196a8e55
recitativonika <fool-body-cheddar@duck.com>
2024-11-30 14:28:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // config.js
 module.exports = {
     ethAmountRange: {
-        min: 0.00000001,
-        max: 0.0000001
+        min: 0.0000001,
+        max: 0.000001
     },
-    delay: 60000 // 1 minute in milliseconds
+    delay: 21600000 // in milliseconds
 };
