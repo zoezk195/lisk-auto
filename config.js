5511eb3ff8802767f958f8a96b9e7a295c6ab67b
recitativonika <fool-body-cheddar@duck.com>
2024-11-30 14:46:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ module.exports = {
         min: 0.00000001,
         max: 0.0000001
     },
-    delay: 21600000 // in milliseconds
+    delay: 21600000, // 6 hour in milliseconds
+    unwarpPercentage: 0.95 // 95% of the wrapped amounts
 };
