a8dc5eee1324ddb5566e9a180f8b31adfd475af1
recitativonika <fool-body-cheddar@duck.com>
2024-12-31 01:16:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
     delay: 21600000, // 6 hours in milliseconds
     unwarpPercentage: 0.95, // 95% of the wrapped amounts
     usdtAmounts: {
-        borrowAmount: 0.16, // Custom borrow amount in USDT (must be more than 0.16)
-        repayAmount: 0.16   // Custom repay amount in USDT (must be more than 0.16)
+        borrowAmount: '0.16', // Custom borrow amount in USDT (must be more than 0.16)
+        repayAmount: '0.16'   // Custom repay amount in USDT (must be more than 0.16)
     }
 };
