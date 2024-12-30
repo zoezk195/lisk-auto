59477ec79aef13d175223c223d747995d34eaa88
recitativonika <fool-body-cheddar@duck.com>
2024-12-31 01:11:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     ethAmountRange: {
         min: 0.00000001, // minimum ETH amount
-        max: 0.0000001 // maximum ETH amount
+        max: 0.0000001  // maximum ETH amount
     },
-    delay: 21600000, // // 6 hour in milliseconds
-    unwarpPercentage: 0.95 // 95% of the wrapped amounts
+    delay: 21600000, // 6 hours in milliseconds
+    unwarpPercentage: 0.95, // 95% of the wrapped amounts
     usdtAmounts: {
-        borrowAmount: '0.16', // Custom borrow amount in USDT (must be more than 0.16)
-        repayAmount: '0.16'   // Custom repay amount in USDT (must be more than 0.16)
+        borrowAmount: 0.16, // Custom borrow amount in USDT (must be more than 0.16)
+        repayAmount: 0.16   // Custom repay amount in USDT (must be more than 0.16)
     }
 };
